fd7e0f86caf5165c2c8a31e0cb530fd2de7db494
DevGusta2 <gustavo.souza2979@gmail.com>
2023-04-18 19:48:32 -0300

    Foram adicionadas as secções na página inicial.

---
+++ ZeroUm/javaScript/index.js
@@ -0,0 +1,77 @@
+//variaveis do Header
+var header=document.getElementById('header');
+var navHeader=document.getElementById('nav_header');
+var btn_menu_header=document.getElementById('btnMenuHeader');
+var content=document.getElementById('content');
+
+var showSidebar=false;
+
+//funções Header responsivo
+function toggleSidebar(){   
+    showSidebar=true;
+
+    if(showSidebar==true){
+        navHeader.style.marginLeft='-10vw';
+        navHeader.style.animationName='showSidebar';
+        btn_menu_header.style.marginLeft='10em';
+        navHeader.style.animationName='showSidebar';
+      
+    }else{
+        navHeader.style.marginLeft='-100vw';
+        navHeader.style.animationName='showSidebar'
+    }
+    
+}
+
+function disbleSidebar(){
+    showSidebar=false;
+    if(showSidebar!=true){
+        navHeader.style.marginLeft='-100vw';
+        navHeader.style.animationName=''
+    }
+}
+function closeSidebar(){
+    
+    if(showSidebar){
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event){
+    if(window.innerWidth > 768 && showSidebar){
+        disbleSidebar();
+    }
+});
+
+
+//teste Animation Color Bar
+var linhaColorida=document.getElementById('AnimationLine');
+var textServicos=document.getElementById('servicosText');
+
+//efeito observerIntersection (Efeito de aparecer os itens quando estiver visivel na tela)
+
+const observer= new IntersectionObserver(entries=>{
+    console.log(entries);
+    Array.from(entries).forEach(entry=>{
+        if(entry.intersectionRatio>=1){
+            entry.target.classList.add('init-hidden-off');
+
+            linhaColorida.style.animationName='pfv';
+            textServicos.style.position='absolute';
+            
+            // linhaColorida.style.height='30em';
+        }
+    
+    })
+},{
+    threshold:[0,0.5,1]
+})
+    
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element=>{
+    observer.observe(element);
+}
+    )
+    
+document.querySelectorAll()
+
+
