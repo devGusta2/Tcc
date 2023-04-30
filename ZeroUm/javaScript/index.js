db4a13499e9ec75a7c82fa31d46418f220a919e3
DevGusta2 <gustavo.souza2979@gmail.com>
2023-04-30 12:58:17 -0300

    Foi adicionado os conteúdos da secção de serviços e também adicionado nova mecânica na barra de scroll.

--- ZeroUm/javaScript/index.js
+++ ZeroUm/javaScript/index.js
@@ -86,14 +86,23 @@ function indicaProgresso() {
     if(rolagem>10){
         document.getElementById("scroll-progresso").style.animationName='line1';
         document.getElementById("icon-mala").style.opacity='1';
+    }else{
+        document.getElementById("scroll-progresso").style.animationName='line2';
+        document.getElementById("icon-mala").style.opacity='0'; 
     }
     if(rolagem>20){
         document.getElementById("scroll-progresso1").style.animationName='line1';
         document.getElementById("icon-tool").style.opacity='1'; 
+    }else{
+        document.getElementById("scroll-progresso1").style.animationName='line2';
+        document.getElementById("icon-tool").style.opacity='0'; 
     }
     if(rolagem>30){
         document.getElementById("scroll-progresso2").style.animationName='line1';
         document.getElementById("icon-video").style.opacity='1'; 
+    }else{
+        document.getElementById("scroll-progresso2").style.animationName='line2';
+        document.getElementById("icon-video").style.opacity='0'; 
     }
     console.log(10*rolagem+"%");
 
